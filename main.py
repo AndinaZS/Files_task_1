71614278358e82fba9d325f0b965a2a6e30e1615
AndinaZS <alexa.markova@outlook.com>
2021-11-20 12:46:25 +0300

    add gt_shop_list

--- main.py
+++ main.py
@@ -16,6 +16,16 @@ def recipes_dict(path):
             file.readline()
         return recipes_dict
 
+def get_shop_list_by_dishes(dishes, person_count):
+    recipes = recipes_dict(os.path.join(os.getcwd(), 'recipes.txt'))
+    result = {}
+    for dish in dishes:
+        recipe = recipes[dish]
+        for el in recipe:
+            ingredient, measure, number  = el['ingredient'], el['measure'], int(el['number'])
+            result.setdefault(ingredient, {})
+            result[ingredient] = {'measure': measure, 'number': result[ingredient].get('number', 0) + number * person_count}
+    return result
 
-recipes = recipes_dict(os.path.join(os.getcwd(), 'recipes.txt'))
-pp(recipes)
+
+pp(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
