90cbc05ec9978efa14066fbcdc2b3fc5c346ac01
AndinaZS <alexa.markova@outlook.com>
2021-11-20 12:06:21 +0300

    add function recipes_dict

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 import os
 from pprint import pprint as pp
-path = os.path.join(os.getcwd(), 'recipes.txt')
-with open(path) as file:
-    recipes_dict = {}
-    for f in file:
-        dish = f.strip()
-        count = int(file.readline().strip())
-        ingredients = []
-        for _ in range(count):
-            ingredient, number, measure = file.readline().strip().split('|')
-            elem = {'ingredient': ingredient, 'number': number, 'measure': measure}
-            ingredients.append(elem)
-        recipes_dict[dish] = ingredients
-        file.readline()
-    pp(recipes_dict)
+
+def recipes_dict(path):
+    with open(path) as file:
+        recipes_dict = {}
+        for f in file:
+            dish = f.strip()
+            count = int(file.readline().strip())
+            ingredients = []
+            for _ in range(count):
+                ingredient, number, measure = file.readline().strip().split('|')
+                elem = {'ingredient': ingredient, 'number': number, 'measure': measure}
+                ingredients.append(elem)
+            recipes_dict[dish] = ingredients
+            file.readline()
+        return recipes_dict
 
 
+recipes = recipes_dict(os.path.join(os.getcwd(), 'recipes.txt'))
+pp(recipes)
